f06811d11e0bb88d51d65748098ee14cc6be7e01
Julio Pena <julio_master_55@hotmail.com>
2020-04-05 15:30:08 -0400

    solution of exercise 5.4

--- exercises/chapter5/exercise_5_4.py
+++ exercises/chapter5/exercise_5_4.py
@@ -21,3 +21,38 @@ recurse(3, 0)
 2) Write a docstring that explains everything someone would need to know in 
 order to use this function (and nothing else).
 """
+
+# Stack Diagram
+#
+#   __main__    |                 |
+#   recurse     | n -> 3   s -> 0 |
+#   recurse     | n -> 2   s -> 3 |
+#   recurse     | n -> 1   s -> 5 |
+#   recurse     | n -> 0   s -> 6 |
+
+
+# Answers
+
+# What is the output of the following program?
+#   The programs print 6
+
+# What would happen if you called this function like this: recurse(-1, 0)?
+#   The program throws an error because the function will be called infinitly,
+#   because n always be lower than 0
+
+
+def recurse(n, s):
+    """
+    Perform the sum of numbers from 1 to 'n' and print the result of sum
+    it with 's'.
+
+    n: int -> must be greater that zero.
+    s: int -> must be greater that equals zero.
+    """
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+
+recurse(3, 0)
