ddf2e40ab0b4bf2fba32e80e880ee35fb3856c9c
JapoDeveloper <julio_master_55@hotmail.com>
2020-04-04 22:12:34 -0400

    solution of exercise 5.1

--- exercises/chapter5/exercise_5_1.py
+++ exercises/chapter5/exercise_5_1.py
@@ -16,3 +16,33 @@ reference point. On UNIX systems, the epoch is 1 January 1970.
 Write a script that reads the current time and converts it to a time of 
 day in hours, minutes, and seconds, plus the number of days since the epoch.
 """
+
+import time
+
+day_secs = 24 * 60 * 60
+hour_secs = 60 * 60
+min_secs = 60
+
+seconds_since_epoch = time.time()
+
+# Option 1: print the time as a combination of units
+days = int(seconds_since_epoch // day_secs)
+remaining = seconds_since_epoch - (days * day_secs)
+
+hours = int(remaining // hour_secs)
+remaining = remaining - (hours * hour_secs)
+
+minutes = int(remaining // min_secs)
+remaining = remaining - (minutes * min_secs)
+
+print(f'Since epoch elapsed {days} days, {hours} hours, {minutes} minutes and {remaining:.2f} seconds')
+
+# Option 2: print the time in every unit independently
+days = int(seconds_since_epoch // day_secs)
+hours = int((seconds_since_epoch // hour_secs))
+minutes = int((seconds_since_epoch // min_secs))
+print('Since epoch elapsed:')
+print(f'{days} days')
+print(f'{hours} hours')
+print(f'{minutes} minutes')
+print(f'{int(seconds_since_epoch)} seconds')
