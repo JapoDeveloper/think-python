264fbe5fa4376dd47bba365057bc13b728a02946
Julio Pena <julio_master_55@hotmail.com>
2020-04-05 11:48:26 -0400

    solution for exercise 5.2

--- exercises/chapter5/exercise_5_2.py
+++ exercises/chapter5/exercise_5_2.py
@@ -17,3 +17,30 @@ Otherwise the program should print, “No, that doesn’t work.”
 converts them to integers, and uses check_fermat to check whether they violate 
 Fermat’s theorem.
 """
+
+
+def check_fermat(a, b, c, n):
+    """
+    Checker for the Fermat's Last Theorem
+    """
+    if n > 2 and ((a**n + b**n) == c**n):
+        print('Holy smokes, Fermat was wrong!')
+    else:
+        print('No, that doesn’t work.')
+
+
+def get_numbers():
+    """
+    Capture user input for values to check de Fermat's Theorem
+    """
+    print('Hello, you are in Fermat\'s Last Theorem checker')
+    print('Type:')
+    a = int(input('a='))
+    b = int(input('b='))
+    c = int(input('c='))
+    n = int(input('n='))
+    return (a, b, c, n)
+
+
+a, b, c, n = get_numbers()
+check_fermat(a, b, c, n)
