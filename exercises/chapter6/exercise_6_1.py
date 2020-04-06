518c7cc7e677bcf518005dc205bea89c914fd724
Julio Pena <julio_master_55@hotmail.com>
2020-04-05 20:23:09 -0400

    solution of exercise 6.1

--- exercises/chapter6/exercise_6_1.py
+++ exercises/chapter6/exercise_6_1.py
@@ -25,3 +25,18 @@ x = 1
 y = x + 1
 print(c(x, y+3, x+y))
 """
+
+# Stack diagram
+
+#   __main__    | x -> 1   y -> 2                                        |
+#   c           | x -> 1   y -> 5   z -> 3   total -> 9   square -> 8100 |
+#   b           | z -> 9   prod -> 90                                    |
+#   a           | x -> 10   y -> 9                                       |
+
+
+# Answer
+
+# What does the program print?
+#   The program print:
+#       9 90 in the first line
+#       8100 in the seconf line
