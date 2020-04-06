829feb4bcbcbc1841a80acdbc560117ab334250a
Julio Pena <julio_master_55@hotmail.com>
2020-04-05 21:03:25 -0400

    solution of exercise 6.3

--- exercises/chapter6/exercise_6_3.py
+++ exercises/chapter6/exercise_6_3.py
@@ -29,3 +29,36 @@ What about the empty string, which is written '' and contains no letters?
 True if it is a palindrome and False otherwise. Remember that you can use the 
 built-in function len to check the length of a string.
 """
+
+from palindrome import *
+
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    elif first(word) == last(word):
+        return is_palindrome(middle(word))
+    else:
+        return False
+
+
+print(first('Hello'))  # H
+print(middle('Hello'))  # ell
+print(last('Hello'))   # o
+
+# What happens if you call middle with a string with two letters?
+print(middle('ab'))  # empty string is returned
+
+# One letter?
+print(middle('a'))  # empty string is returned
+
+# What about the empty string, which is written '' and contains no letters?
+print(middle(''))  # empty string is returned
+
+print('Are palindromes?:')
+print('noon', is_palindrome('noon'))  # Yes
+print('redivider', is_palindrome('redivider'))  # Yes
+print('a', is_palindrome('a'))  # Yes
+print('empty string', is_palindrome(''))  # Yes
+print('night', is_palindrome('night'))  # No
+print('word', is_palindrome('word'))  # No
---
+++ exercises/chapter6/palindrome.py
@@ -0,0 +1,13 @@
+def first(word):
+    """Returns the first character of a string."""
+    return word[0]
+
+
+def last(word):
+    """Returns the last of a string."""
+    return word[-1]
+
+
+def middle(word):
+    """Returns all but the first and last characters of a string."""
+    return word[1:-1]
