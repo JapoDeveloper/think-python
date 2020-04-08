a1d512cb42fe9a8d7ce6411f73b3a6552627c2b1
Julio Pena <julio_master_55@hotmail.com>
2020-04-07 23:43:21 -0400

    improve legibility of exercise 7.3

--- exercises/chapter7/exercise_7_3.py
+++ exercises/chapter7/exercise_7_3.py
@@ -37,8 +37,9 @@ def estimate_pi():
     epsilon = 1e-15
     factor = 2 * math.sqrt(2) / 9801
     while True:
-        term = factor * (factorial(4*k) * (1103 + 26390 * k)
-                         ) / (factorial(k)**4 * 396**(4*k))
+        num = factorial(4*k) * (1103 + 26390 * k)
+        den = factorial(k)**4 * 396**(4*k)
+        term = factor * num / den
         total += term
         if term < epsilon:
             break
