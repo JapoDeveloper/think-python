551f3838e206f9f5299c747b66014de26b4720de
Julio Pena <julio_master_55@hotmail.com>
2020-04-07 23:39:49 -0400

    solution of exercise 7.3

--- exercises/chapter7/exercise_7_3.py
+++ exercises/chapter7/exercise_7_3.py
@@ -16,3 +16,35 @@ summation until the last term is smaller than 1e-15
 (which is Python notation for 10**-15). 
 You can check the result by comparing it to math.pi.
 """
+
+import math
+
+
+def factorial(n):
+    """calculate the factorial of a non-negative integer number"""
+    if type(n) is not int or n < 0:
+        print('Only non-negative integers are accepted.')
+        return
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+
+def estimate_pi():
+    """compute an approximation of pi"""
+    k = 0
+    total = 0
+    epsilon = 1e-15
+    factor = 2 * math.sqrt(2) / 9801
+    while True:
+        term = factor * (factorial(4*k) * (1103 + 26390 * k)
+                         ) / (factorial(k)**4 * 396**(4*k))
+        total += term
+        if term < epsilon:
+            break
+        k += 1
+    return 1 / total
+
+
+print('Estimation of pi    :', estimate_pi())
+print('Math module pi value:', math.pi)
