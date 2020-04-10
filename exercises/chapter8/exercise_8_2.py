69aea508feca098c56d6977cfbc03cb26ba6fe90
Julio Pena <julio_master_55@hotmail.com>
2020-04-10 16:09:12 -0400

    add files to solutions of exercises in chapter 8

---
+++ exercises/chapter8/exercise_8_2.py
@@ -0,0 +1,10 @@
+"""
+Think Python, 2nd Edition
+Chapter 8
+Exercise 8.2
+
+Description:
+There is a string method called count that is similar to the function in 
+“Looping and Counting”. Read the documentation of this method and write an 
+invocation that counts the number of a’s in 'banana'.
+"""
---
+++ exercises/chapter8/exercise_8_3.py
@@ -0,0 +1,19 @@
+"""
+Think Python, 2nd Edition
+Chapter 8
+Exercise 8.3
+
+Description:
+A string slice can take a third index that specifies the “step size”; that is, 
+the number of spaces between successive characters. A step size of 2 means 
+every other character; 3 means every third, etc.
+
+    >>> fruit = 'banana'
+    >>> fruit[0:5:2]
+    'bnn'
+
+A step size of -1 goes through the word backwards, so the slice [::-1] 
+generates a reversed string.
+
+Use this idiom to write a one-line version of is_palindrome from Exercise 6-3.
+"""
