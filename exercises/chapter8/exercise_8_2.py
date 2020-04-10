b34b07902b74687ca3244b18f3d483cbf6c1a719
Julio Pena <julio_master_55@hotmail.com>
2020-04-10 18:18:33 -0400

    solution of exercise 8.2

--- exercises/chapter8/exercise_8_2.py
+++ exercises/chapter8/exercise_8_2.py
@@ -8,3 +8,5 @@ There is a string method called count that is similar to the function in
 “Looping and Counting”. Read the documentation of this method and write an 
 invocation that counts the number of a’s in 'banana'.
 """
+
+print('The word \'banana\' has {} a\'s'.format('banana'.count('a')))
